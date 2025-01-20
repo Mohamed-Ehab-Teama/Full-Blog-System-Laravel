daccbbb403c57fad3ebf7084b2140c8539af4aa5
Mohamed Ehab Mohamed Teama <150657955+Mohamed-Ehab-Teama@users.noreply.github.com>
2025-01-20 16:46:44 +0200

    Authentication part Completion

--- resources/views/theme/register.blade.php
+++ resources/views/theme/register.blade.php
@@ -15,24 +15,34 @@
           @csrf
           <div class="row">
             <div class="col-6">
+
               <div class="form-group">
                 <input class="form-control border" name="name" value="{{ old('name') }}" type="text" placeholder="Enter your name">
+                <x-input-error :messages="$errors->get('name')" class="mt-2" />
               </div>
+
               <div class="form-group">
                 <input class="form-control border" name="email" value="{{ old('email') }}" type="email" placeholder="Enter email address">
+                <x-input-error :messages="$errors->get('email')" class="mt-2" />
               </div>
+
             </div>
             <div class="col-6">
+
               <div class="form-group">
                 <input class="form-control border" name="password" type="password" placeholder="Enter your password">
+                <x-input-error :messages="$errors->get('password')" class="mt-2" />
               </div>
+
               <div class="form-group">
                 <input class="form-control border" name="password_confirmation" type="password" placeholder="Enter your password confirmation">
+                <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
               </div>
+
             </div>
           </div>
           <div class="form-group text-center text-md-right mt-3">
-            <a href="{{ route('login') }}" class="mx-3" >Already have an account ! </a>
+            <a href="{{ route('login') }}" class="mx-3">Already have an account ! </a>
             <button type="submit" class="button button--active button-contactForm">Register</button>
           </div>
         </form>
