ce75679641ea0f02ae0dfca1117c9d64468b1fef
Mohamed Ehab Mohamed Teama <mohamed111.me111@gmail.com>
2025-01-22 07:50:03 +0200

    Categories Module Completed

--- resources/views/theme/partials/slider.blade.php
+++ resources/views/theme/partials/slider.blade.php
@@ -1,3 +1,8 @@
+@php
+    $sideBarCategories = App\Models\Category::get();
+@endphp
+
+
 <!-- Start Blog Post Siddebar -->
 <div class="col-lg-4 sidebar-widgets">
     <div class="widget-wrap">
@@ -26,38 +31,18 @@
 
         <div class="single-sidebar-widget post-category-widget">
             <h4 class="single-sidebar-widget__title">Catgory</h4>
-            <ul class="cat-list mt-20">
-                <li>
-                    <a href="#" class="d-flex justify-content-between">
-                        <p>Technology</p>
-                        <p>(03)</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" class="d-flex justify-content-between">
-                        <p>Software</p>
-                        <p>(09)</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" class="d-flex justify-content-between">
-                        <p>Lifestyle</p>
-                        <p>(12)</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" class="d-flex justify-content-between">
-                        <p>Shopping</p>
-                        <p>(02)</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" class="d-flex justify-content-between">
-                        <p>Food</p>
-                        <p>(10)</p>
-                    </a>
-                </li>
-            </ul>
+            @if (count($sideBarCategories) > 0)
+                <ul class="cat-list mt-20">
+                    @foreach ($sideBarCategories as $category)
+                        <li>
+                            <a href="#" class="d-flex justify-content-between">
+                                <p> {{ $category->name }} </p>
+                                <p>(03)</p>
+                            </a>
+                        </li>
+                    @endforeach
+                </ul>
+            @endif
         </div>
 
         <div class="single-sidebar-widget popular-post-widget">
