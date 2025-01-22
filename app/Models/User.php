7c58f4e70d8f73828199ba7430884b16451323e3
Mohamed Ehab Mohamed Teama <mohamed111.me111@gmail.com>
2025-01-22 10:11:05 +0200

    to push

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,12 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+
+
+    public function blogs()
+    {
+        return $this->hasMany(Blog::class);
+    }
+
 }
