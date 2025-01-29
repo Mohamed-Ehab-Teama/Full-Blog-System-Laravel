6478a92f88c81a4acfd2cb3cf6a7919781ef4de0
Mohamed Ehab Mohamed Teama <mohamed111.me111@gmail.com>
2025-01-29 13:03:50 +0200

    category count + recent blogs + date of recent categories

--- resources/views/theme/index.blade.php
+++ resources/views/theme/index.blade.php
@@ -28,66 +28,24 @@
         <section>
             <div class="container">
                 <div class="owl-carousel owl-theme blog-slider">
-                    <div class="card blog__slide text-center">
-                        <div class="blog__slide__img">
-                            <img class="card-img rounded-0" src="{{asset('assets')}}/img/blog/blog-slider/blog-slide1.png" alt="">
-                        </div>
-                        <div class="blog__slide__content">
-                            <a class="blog__slide__label" href="#">Fashion</a>
-                            <h3><a href="#">New york fashion week's continued the evolution</a></h3>
-                            <p>2 days ago</p>
-                        </div>
-                    </div>
-                    <div class="card blog__slide text-center">
-                        <div class="blog__slide__img">
-                            <img class="card-img rounded-0" src="{{asset('assets')}}/img/blog/blog-slider/blog-slide2.png" alt="">
-                        </div>
-                        <div class="blog__slide__content">
-                            <a class="blog__slide__label" href="#">Fashion</a>
-                            <h3><a href="#">New york fashion week's continued the evolution</a></h3>
-                            <p>2 days ago</p>
-                        </div>
-                    </div>
-                    <div class="card blog__slide text-center">
-                        <div class="blog__slide__img">
-                            <img class="card-img rounded-0" src="{{asset('assets')}}/img/blog/blog-slider/blog-slide3.png" alt="">
-                        </div>
-                        <div class="blog__slide__content">
-                            <a class="blog__slide__label" href="#">Fashion</a>
-                            <h3><a href="#">New york fashion week's continued the evolution</a></h3>
-                            <p>2 days ago</p>
-                        </div>
-                    </div>
-                    <div class="card blog__slide text-center">
-                        <div class="blog__slide__img">
-                            <img class="card-img rounded-0" src="{{asset('assets')}}/img/blog/blog-slider/blog-slide1.png" alt="">
-                        </div>
-                        <div class="blog__slide__content">
-                            <a class="blog__slide__label" href="#">Fashion</a>
-                            <h3><a href="#">New york fashion week's continued the evolution</a></h3>
-                            <p>2 days ago</p>
-                        </div>
-                    </div>
-                    <div class="card blog__slide text-center">
-                        <div class="blog__slide__img">
-                            <img class="card-img rounded-0" src="{{asset('assets')}}/img/blog/blog-slider/blog-slide2.png" alt="">
-                        </div>
-                        <div class="blog__slide__content">
-                            <a class="blog__slide__label" href="#">Fashion</a>
-                            <h3><a href="#">New york fashion week's continued the evolution</a></h3>
-                            <p>2 days ago</p>
-                        </div>
-                    </div>
-                    <div class="card blog__slide text-center">
-                        <div class="blog__slide__img">
-                            <img class="card-img rounded-0" src="{{asset('assets')}}/img/blog/blog-slider/blog-slide3.png" alt="">
-                        </div>
-                        <div class="blog__slide__content">
-                            <a class="blog__slide__label" href="#">Fashion</a>
-                            <h3><a href="#">New york fashion week's continued the evolution</a></h3>
-                            <p>2 days ago</p>
-                        </div>
-                    </div>
+
+                    @if (count($allBlogs) > 0)
+                        @foreach ($allBlogs as $blog)
+                            <div class="card blog__slide text-center">
+                                <div class="blog__slide__img">
+                                    <img class="card-img rounded-0"
+                                        src="{{ asset("storage/blogs/$blog->image") }}" alt="">
+                                </div>
+                                <div class="blog__slide__content">
+                                    <a class="blog__slide__label" href="#"> {{$blog->name}} </a>
+                                    <h3><a href="#"> {{$blog->description}} </a></h3>
+                                    <p> {{ date('d') - $blog->updated_at->format('d') }} days ago</p>
+                                </div>
+                            </div>
+                        @endforeach
+                    @endif
+
+
                 </div>
             </div>
         </section>
